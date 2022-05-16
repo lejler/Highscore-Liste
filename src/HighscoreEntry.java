3f898bfa1d1fe3293c85a31fc41e05291c3f18fc
llayla <layla.shokri@gmx.de>
2022-05-16 16:42:46 +0200

    setup

---
+++ src/HighscoreEntry.java
@@ -0,0 +1,27 @@
+public class HighscoreEntry {
+    
+    private String nickname;
+    private int score;
+
+    public HighscoreEntry(String nickname, int score) {
+        this.nickname = nickname;
+        this.score = score;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+}
